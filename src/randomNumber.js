244607a5cef12ce31f522ccc11f805d3e6551d53
PavelMakin <muravyev.pavel@hexly.ru>
2023-02-01 13:22:46 +0300

    add fuction getRandomNumber

---
+++ src/randomNumber.js
@@ -0,0 +1,6 @@
+export default function getRandomNumber(max, min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(getRandomNumber(1, 100))
