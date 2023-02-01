7206e540731494799147dd55234109aaf152e30b
PavelMakin <muravyev.pavel@hexly.ru>
2023-02-01 13:23:45 +0300

    debag code

--- src/randomNumber.js
+++ src/randomNumber.js
@@ -2,5 +2,4 @@ export default function getRandomNumber(max, min) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-console.log(getRandomNumber(1, 100))
+};
