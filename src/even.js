606a82e97f4f7a346a4a30e3886e5eec9d739a59
PavelMakin <muravyev.pavel@hexly.ru>
2023-02-07 12:56:09 +0300

    corect code

--- src/even.js
+++ src/even.js
@@ -1,5 +1,6 @@
 import consistentTypeSpecifierStyle from 'eslint-plugin-import/lib/rules/consistent-type-specifier-style.js';
-import { getRandomNumber } from '../randomNumbers.js';
+import getRandomNumber from './randomNumber.js';
+import outputText from './output.js';
 
 const introductoryText = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -12,10 +13,11 @@ function outputGenerator() {
     const number = getRandomNumber(minRange, maxRange);
     const parity = (number) => number % 2 === 0;
     const corectAnswer = parity(number) ? 'yes' : 'no';
+    const question = (number);
 
-    return (number, corectAnswer);
+    return [question, corectAnswer];
 };
 
-function parityAnswers () {
-
+export default function derivationCorrectAnswer () {
+    outputText(introductoryText, outputGenerator);
 };
