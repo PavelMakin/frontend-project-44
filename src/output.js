707ab1e2cdad4e6af2fb3890e887a123f57f14f7
PavelMakin <muravyev.pavel@hexly.ru>
2023-02-07 12:56:33 +0300

    add outputText function

---
+++ src/output.js
@@ -0,0 +1,25 @@
+import readlineSync from 'readline-sync';
+
+const stepCount = 3;
+
+export default function outputText (introductoryText, outputGenerator) {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}`);
+    console.log(introductoryText);
+
+    for (let i = 0; i < stepCount; i++){
+        const [question, corectAnswer] = outputGenerator();
+        console.log(`Question: ${question}`);
+        const userAnsver = readlineSync.question('Your answer?: ');
+        if (corectAnswer !== userAnsver) {
+            console.log(`'${userAnsver}' is wrong answer ;(. Corect answer was '${corectAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`)
+
+            return;
+        }
+        console.log('Correct!')
+    }
+    console.log(`Congratulations, ${userName}`)
+};
+
